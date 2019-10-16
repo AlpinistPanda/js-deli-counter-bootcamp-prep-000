99d25cf55b49ab8be4d6d3dd584ec3724a6c7ea0
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-16 23:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(line) {
   else {
     var str = "The line is currently: "
     for(var i = 0; i < line.length; i++) {
-      str = str + i + ". " + line[i] + i<line.length-1 ? ',' : '';
+      str = str + (i+1) + ". " + line[i] + i<line.length-1 ? ',' : '';
     }
     return str
   }
